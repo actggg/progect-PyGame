db4251434bc412763a0a41f9c58263bb02a2494a
yur.raz@mail.ru <yur.raz@mail.ru>
2023-01-02 14:16:47 +0300

    ракеты попытка 4

--- машинка.py
+++ машинка.py
@@ -1,12 +1,14 @@
 import sys
 import pygame
 import os
+from time import sleep
 
 pygame.init()
 FPS = 50
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+money = 10
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -48,6 +50,54 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
+                menu()
+                return 0
+        pygame.display.flip()
+        clock.tick(FPS)
+
+def menu():
+
+    intro_text = ['ddsfasfsa', 'dddd', 'ddd']
+    font = pygame.font.SysFont('Consolas', 38)
+
+    clock = pygame.time.Clock()
+    pygame.draw.rect(screen, 'black',
+                     (0, 0, width, 40))
+    fon = font.render(str(money), 10, pygame.Color('yellow'))
+    screen.blit(fon, (width - len(str(money)) * 38 - 60, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    screen.blit(font.render(str(money), False, 'yellow'), (width, 0))
+    '''
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    '''
+    pygame.draw.rect(screen, 'yellow',
+                     (20, 20, 100, 75))
+    pygame.draw.rect(screen, 'yellow',
+                     (width - 45, 3, 30, 30))
+    pygame.draw.line(screen, 'black',
+                     [width - 40, 17],
+                     [width - 20, 17], 3)
+    pygame.draw.line(screen, 'black',
+                     [width - 30, 7],
+                     [width - 30, 27], 3)
+    image = load_image('обрезанная монета.png', 'black')
+    dog_rect = image.get_rect(
+        bottomright=(width - len(str(money)) * 10 - 30, image.get_height() + 2))
+    screen.blit(image, dog_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            #elif event.type == pygame.KEYDOWN or \
+                    #event.type == pygame.MOUSEBUTTONDOWN:
                 return 0
         pygame.display.flip()
         clock.tick(FPS)
@@ -67,7 +117,7 @@ def load_level(filename):
 tile_images = {
     'wall': load_image('кирпичи.jpg'),
     'empty': load_image('земля.jpeg'),
-    'coin': load_image('монета.jpg'),
+    'coin': load_image('star.png'),
     'exit': load_image('сундук1.png', -1)
 }
 player_image = load_image('герой (4).png')
@@ -88,6 +138,10 @@ class Money(pygame.sprite.Sprite):
             self.image = tile_images[tile_type]
             self.rect = self.image.get_rect().move(
                 tile_width * pos_x, tile_height * pos_y)
+            if tile_type == 'coin':
+                self.price = 1
+            else:
+                self.price = 10
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
@@ -109,6 +163,14 @@ tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 money_group = pygame.sprite.Group()
 
+def draw(screen, number):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render(str(number), True, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    screen.blit(text, (text_x, text_y))
+
 def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
@@ -127,11 +189,12 @@ def generate_level(level):
                 Tile('empty', x, y)
                 new_player = Player(x, y)
     return new_player, x, y
-
 pygame.display.set_caption('Перемещение героя')
 running = True
 start_screen()
+menu()
 level_map = load_level('map.txt')
+font = pygame.font.SysFont('Consolas', 30)
 player, level_x, level_y = generate_level(load_level('map.txt'))
 while running:
     for event in pygame.event.get():
@@ -145,6 +208,7 @@ while running:
                         x, y = x, y - 1
                         player.go(x, y)
                         if pygame.sprite.spritecollideany(player, money_group):
+                            money += pygame.sprite.spritecollideany(player, money_group).price
                             pygame.sprite.spritecollideany(player, money_group).kill()
             if event.key == pygame.K_DOWN:
                 if y < level_y - 1 and level_map[y + 1][x] != '#':
@@ -152,6 +216,7 @@ while running:
                         x, y = x, y + 1
                         player.go(x, y)
                         if pygame.sprite.spritecollideany(player, money_group):
+                            money += pygame.sprite.spritecollideany(player, money_group).price
                             pygame.sprite.spritecollideany(player, money_group).kill()
             if event.key == pygame.K_LEFT:
                 if x > 0 and level_map[y][x - 1] != '#':
@@ -159,6 +224,7 @@ while running:
                         x, y = x - 1, y
                         player.go(x, y)
                         if pygame.sprite.spritecollideany(player, money_group):
+                            money += pygame.sprite.spritecollideany(player, money_group).price
                             pygame.sprite.spritecollideany(player, money_group).kill()
             if event.key == pygame.K_RIGHT:
                 if x < level_x - 1 and level_map[y][x + 1] != '#':
@@ -166,12 +232,16 @@ while running:
                         x, y = x + 1, y
                         player.go(x, y)
                         if pygame.sprite.spritecollideany(player, money_group):
+                            money += pygame.sprite.spritecollideany(player, money_group).price
                             pygame.sprite.spritecollideany(player, money_group).kill()
+
+                            #number += 1
                         #if level_map[y][x] == '+':
 
             #if level_map[y][x] == 'x':
                 #player.kill()
     tiles_group.draw(screen)
     player_group.draw(screen)
+    screen.blit(font.render(str(money), True, (0, 0, 0)), (width - 100, 0))
     pygame.display.flip()
 pygame.quit()
